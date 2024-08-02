4de68dabc7eb082cda83ebaef9291d1f8357d85e
LouisCR04 <louiskariuki04@gmail.com>
2024-08-02 16:18:23 +0300

    Init Commit: Redis

--- newTaskscripter.sh
+++ newTaskscripter.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "File name?"
 read file
-echo -e "#!/usr/bin/env python3\n# $file\n\n\"\"\" Pagination Exercises \"\"\"" > $file
+echo -e "#!/usr/bin/env python3\n# $file\n\n\"\"\" i18n (Internationalization) Exercises \"\"\"" > $file
 chmod u+x $file
 vim $file
